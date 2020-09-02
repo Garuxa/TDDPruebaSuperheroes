6e1dfc0a66a4357dc389e26809b389c2e5c17391
Gary Hermosilla <garuxa@gmail.com>
2020-09-02 11:47:22 -0400

    Creacion de archivo kotlin SuperHeroDataBase Implementación de Room database Singleton definido para prevenir que se creen múltiples instancias de la base de datos al mismo tiempo

---
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/db/SuperHeroDataBase.kt
@@ -0,0 +1,36 @@
+package cl.desafiolatam.tddpruebasuperheroes.model.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = [SuperHeroEntity::class], version = 1, exportSchema = false)
+abstract class SuperHeroRoomDatabase : RoomDatabase() {
+
+    abstract fun superHeroDao(): SuperHeroDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: SuperHeroRoomDatabase? = null
+
+        fun getDatabase(context: Context): SuperHeroRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SuperHeroRoomDatabase::class.java,
+                    "superhero_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
