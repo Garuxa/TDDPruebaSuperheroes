e2c10686f38387a1768cd2730a8254c105e8ab5b
Gary Hermosilla <garuxa@gmail.com>
2020-09-02 12:39:12 -0400

    Archivo ApiConsumer que contiene la interfaz para interactuar con la URL y la clase Retrofit con los parámetros necesarios para que funcione

---
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/remote/ApiConsumer.kt
@@ -0,0 +1,27 @@
+package cl.desafiolatam.tddpruebasuperheroes.model.remote
+
+import cl.desafiolatam.tddpruebasuperheroes.model.remote.pojo.SuperHeroPojo
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface SuperHeroesAPI {
+    @GET("/all.json")
+    fun listaHeroes(): Call<SuperHeroPojo>
+}
+
+class RetrofitClient {
+    companion object {
+        private const val BASE_URL = "https://akabab.github.io/superhero-api/api"
+
+        fun retrofitInstance(): SuperHeroesAPI {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(SuperHeroesAPI::class.java)
+        }
+    }
+}
+
--- app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/remote/pojo/SuperHeroPojo.kt
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/remote/pojo/SuperHeroPojo.kt
@@ -1,9 +1,13 @@
 package cl.desafiolatam.tddpruebasuperheroes.model.remote.pojo
 
 data class SuperHeroPojo(
+    val id: Int,
     val name: String,
+    val slug: String,
+    val powerstats: Map<String, List<String>>,
     val appearance: Map<String, List<String>>,
+    val biography: Map<String, List<String>>,
+    val work: Map<String, List<String>>,
+    val connections: Map<String, List<String>>,
     val images: Map<String, List<String>>
 )
-
-data class SuperHeroImagesPojo(val name: String, val images: List<String>)
