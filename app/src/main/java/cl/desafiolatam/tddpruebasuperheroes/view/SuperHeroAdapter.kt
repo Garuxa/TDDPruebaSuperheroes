dce7691b3401c625abcb73b0e877dc3fb1d27885
Gary Hermosilla <garuxa@gmail.com>
2020-09-03 23:45:07 -0400

    Adaptador para mostrar cada elemento del RecyclerView

---
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/view/SuperHeroAdapter.kt
@@ -0,0 +1,42 @@
+package cl.desafiolatam.tddpruebasuperheroes.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cl.desafiolatam.tddpruebasuperheroes.R
+import cl.desafiolatam.tddpruebasuperheroes.model.remote.pojo.SuperHeroPojo
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.superheroes_list_item.view.*
+
+class SuperHeroAdapter(private var myDataset: List<SuperHeroPojo.SuperHeroPojoItem>) :
+    RecyclerView.Adapter<SuperHeroAdapter.SuperHeroHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SuperHeroHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.superheroes_list_item, parent, false)
+
+        return SuperHeroHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SuperHeroHolder, position: Int) {
+        val superHero = myDataset[position]
+        Picasso.get()
+            .load(superHero.images.md)
+            .into(holder.photo)
+        holder.name_title.text = superHero.name
+    }
+
+    override fun getItemCount(): Int {
+        return myDataset.size
+    }
+
+    class SuperHeroHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var photo: ImageView = itemView.iv_superhero_list
+        var name_title: TextView = itemView.superhero_name_title
+    }
+}
