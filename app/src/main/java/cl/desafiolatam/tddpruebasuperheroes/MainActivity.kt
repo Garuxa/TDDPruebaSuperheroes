9b397896a0d2d1d57312a12d947ba82511d12bf4
Gary Hermosilla <garuxa@gmail.com>
2020-09-03 22:26:31 -0400

    Implementación y pruebas de que funciona Retrofit

--- app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/MainActivity.kt
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/MainActivity.kt
@@ -1,11 +1,42 @@
 package cl.desafiolatam.tddpruebasuperheroes
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import cl.desafiolatam.tddpruebasuperheroes.model.remote.RetrofitClient
+import cl.desafiolatam.tddpruebasuperheroes.model.remote.pojo.SuperHeroPojo
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadApidata()
+    }
+
+    private fun loadApidata() {
+        val service = RetrofitClient.retrofitInstance()
+        val call = service.listaHeroes()
+
+        call.enqueue(object : Callback<SuperHeroPojo> {
+            override fun onResponse(call: Call<SuperHeroPojo>, response: Response<SuperHeroPojo>) {
+                Log.d("MAIN", "${response.code()}")
+                Log.d("MAIN", "${call.request().url()}")
+                Log.d("MAIN", "${response.body()}")
+            }
+
+            override fun onFailure(call: Call<SuperHeroPojo>, t: Throwable) {
+                Log.d("MAIN", "Error " + t)
+                Toast.makeText(
+                    applicationContext,
+                    "Error: no pudimos recuperar los datos desde la api",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
