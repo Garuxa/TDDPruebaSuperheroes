dcf185717497bdb68ff0152ad1bfbe2fa5725f13
Gary Hermosilla <garuxa@gmail.com>
2020-09-04 01:46:02 -0400

    Instancias del RecyclerView, llamados al Retrofit, añadido linea divisora decoratoria para separar cada item del RecyclerView

--- app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/MainActivity.kt
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/MainActivity.kt
@@ -4,18 +4,37 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import cl.desafiolatam.tddpruebasuperheroes.model.remote.RetrofitClient
 import cl.desafiolatam.tddpruebasuperheroes.model.remote.pojo.SuperHeroPojo
+import cl.desafiolatam.tddpruebasuperheroes.view.SuperHeroAdapter
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private var superHeroesList = ArrayList<SuperHeroPojo.SuperHeroPojoItem>()
+    private lateinit var viewAdapter: SuperHeroAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewAdapter = SuperHeroAdapter(superHeroesList)
+        superHero_Recycler.adapter = viewAdapter
+
         loadApidata()
+
+        val picasso = Picasso.Builder(applicationContext)
+            //.indicatorsEnabled(true)
+            .loggingEnabled(true)
+            .build()
+
+        Picasso.setSingletonInstance(picasso)
     }
 
     private fun loadApidata() {
@@ -24,9 +43,21 @@ class MainActivity : AppCompatActivity() {
 
         call.enqueue(object : Callback<SuperHeroPojo> {
             override fun onResponse(call: Call<SuperHeroPojo>, response: Response<SuperHeroPojo>) {
-                Log.d("MAIN", "${response.code()}")
-                Log.d("MAIN", "${call.request().url()}")
-                Log.d("MAIN", "${response.body()}")
+                //Log.d("MAIN", "${response.code()}")
+                //Log.d("MAIN", "${call.request().url()}")
+                //Log.d("MAIN", "${response.body()}")
+                response.body()?.map {
+                    Log.d("MAIN", "${it.id} - ${it.name} - ${it.images.md}")
+                }
+                viewAdapter.updateItems(response.body()!!)
+                //viewAdapter.notifyDataSetChanged()
+
+                superHero_Recycler.addItemDecoration(
+                    DividerItemDecoration(
+                        this@MainActivity,
+                        LinearLayoutManager.VERTICAL
+                    )
+                )
             }
 
             override fun onFailure(call: Call<SuperHeroPojo>, t: Throwable) {
