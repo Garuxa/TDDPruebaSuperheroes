f536e480ed176efbcaf009f73c15996cdd1369de
Gary Hermosilla <garuxa@gmail.com>
2020-09-06 01:19:15 -0300

    - Implementación de la anotación @TypeConverters mediante la clase StringListConverter. - Función renombrada para la llamada del Dao (para mejor manejo del código)

--- app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/db/SuperHeroDataBase.kt
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/db/SuperHeroDataBase.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
 @Database(entities = [SuperHeroEntity::class], version = 1, exportSchema = false)
+@TypeConverters(StringListConverter::class) // MAS INFO EN CLASE StringListConverter
 abstract class SuperHeroRoomDatabase : RoomDatabase() {
 
-    abstract fun superHeroDao(): SuperHeroDao
+    abstract fun getSuperHeroDao(): SuperHeroDao
 
     companion object {
         // Singleton prevents multiple instances of database opening at the
