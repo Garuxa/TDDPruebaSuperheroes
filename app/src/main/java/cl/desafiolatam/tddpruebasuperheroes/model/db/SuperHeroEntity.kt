c75e45d5284b09764087f67b80a8d91e55c027b7
Gary Hermosilla <garuxa@gmail.com>
2020-09-05 20:55:51 -0400

    Se completa el Entity para guardar los datos que se consumiran desde la API

--- app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/db/SuperHeroEntity.kt
+++ app/src/main/java/cl/desafiolatam/tddpruebasuperheroes/model/db/SuperHeroEntity.kt
@@ -1,8 +1,20 @@
 package cl.desafiolatam.tddpruebasuperheroes.model.db
 
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import cl.desafiolatam.tddpruebasuperheroes.model.remote.pojo.SuperHeroPojo
 
-@Entity(tableName = "superHero")
-data class SuperHeroEntity(@PrimaryKey @ColumnInfo(name = "superHero_name") val SuperHeroName: String)
+@Entity(tableName = "supehero_table")
+data class SuperHeroEntity(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val slug: String,
+    @Embedded val powerstats: SuperHeroPojo.SuperHeroPojoItem.Powerstats,
+    @Embedded val appearance: SuperHeroPojo.SuperHeroPojoItem.Appearance,
+    @Embedded val biography: SuperHeroPojo.SuperHeroPojoItem.Biography,
+    @Embedded val work: SuperHeroPojo.SuperHeroPojoItem.Work,
+    @Embedded val connections: SuperHeroPojo.SuperHeroPojoItem.Connections,
+    @Embedded val images: SuperHeroPojo.SuperHeroPojoItem.Images
+)
